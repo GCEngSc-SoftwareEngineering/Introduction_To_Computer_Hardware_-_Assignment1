1a2ad3c2360cf954998c51236c64656ac034460a
Brenton James <261373365+GCEngSc-SoftwareEngineering@users.noreply.github.com>
2026-03-13 17:09:44 +1000

    Add files via upload
    Signed-off-by: Brenton James <261373365+GCEngSc-SoftwareEngineering@users.noreply.github.com>

---
+++ src/main/java/src/CLIBAdder/CLIBAdder.java
@@ -0,0 +1,58 @@
+package src.CLIBAdder;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+import src.Main.Framework_Ptr;
+
+public class CLIBAdder implements LIBADDER_API
+{
+	@Override
+	public double do_Add(Pointer obj) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Pointer generate_Pprogram() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float get_Input_Subset_praise0_valueA(Pointer obj) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float get_Input_Subset_praise0_valueB(Pointer obj) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void set_Input_Subset_praise0_valueA(Pointer obj, float newValue) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set_Input_Subset_praise0_valueB(Pointer obj, float newValue) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void updateFramPtr(Framework_Ptr s) {
+		// TODO Auto-generated method stub
+		
+	}
+}
